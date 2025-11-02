66ffbc112dca632504643364db921a54996c6c06
osamaElsaadany <osamaelsadany58@gmail>
2025-11-02 17:40:31 +0200

    up

--- lib/features/auth/presentation/cubit/auth_cubit.dart
+++ lib/features/auth/presentation/cubit/auth_cubit.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:se7ty/features/auth/models/User_type_enum.dart';
+import 'package:se7ty/features/auth/models/doctor_model.dart';
+import 'package:se7ty/features/auth/models/patient_model.dart';
 import 'package:se7ty/features/auth/presentation/cubit/auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
@@ -21,7 +24,33 @@ class AuthCubit extends Cubit<AuthState> {
             password: passwordController.text,
           );
       User? user = credential.user;
-      user?.updateDisplayName(nameController.text);
+      await user?.updateDisplayName(nameController.text);
+
+
+      if (userType == UserTypeEnum.doctor) {
+        //! user photoURL as Role (doctor , patient)
+        user?.updatePhotoURL('doctor');
+        var doctor = DoctorModel(
+          uid: user?.uid,
+          name: nameController.text,
+          email: emailController.text,
+        );
+        await FirebaseFirestore.instance
+            .collection("doctor")
+            .doc(user?.uid)
+            .set(doctor.toJson());
+      } else {
+        user?.updatePhotoURL('patient');
+        var patient = PatientModel(
+          uid: user?.uid,
+          name: nameController.text,
+          email: emailController.text,
+        );
+        await FirebaseFirestore.instance
+            .collection("patient")
+            .doc(user?.uid)
+            .set(patient.toJson());
+      }
       emit(AuthSuccessState());
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
